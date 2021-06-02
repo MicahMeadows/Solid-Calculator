e733df6d7c523db12b4ff87e08ddb16676c839fc
MicahMeadows <micahstlp@gmail.com>
2021-06-01 22:36:58 -0400

    Add IntCalculator unit test

---
+++ SOLID.Calculator.Tests/IntCalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using SOLID.Calculator.Calculation;
+using SOLID.Calculator.Expressions;
+
+namespace SOLID.Calculator.Tests
+{
+    [TestFixture]
+    class IntCalculatorTest
+    {
+        [Test]
+        public void Test_ProcessCalculation_LhsFiveRhsFiveOperatorPlus_ReturnsTenAsString()
+        {
+            // Given
+            StringExpression stringExpression = new StringExpression { lhs = "5", rhs = "5", op = "+" };
+            IntCalculator intCalculator = new IntCalculator();
+
+            // When
+            string result = intCalculator.ProcessCalculation(stringExpression);
+
+            // Then
+            Assert.AreEqual("10", result);
+        }
+    }
+}
